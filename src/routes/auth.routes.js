471c37ef6767079a5a2193257b81e941ebae4035
Stav Levi <stav.levi@netnut.io>
2024-07-31 12:22:54 +0300

    fix middleware

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,21 @@
+import express from "express";
+const router = express.Router();
+import { generateAccessToken } from "../middleware/auth.js";
+
+router.get("/", async (req, res) => {
+  try {
+    const token = generateAccessToken({ username: "stav" });
+    res.json({
+      success: true,
+      message: "login working",
+      token: token,
+    });
+  } catch (e) {
+    res.json({
+      success: false,
+      error: e.message,
+    });
+  }
+});
+
+export default router;
